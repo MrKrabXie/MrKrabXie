bfa9eaa4c31606510a36a2308df57683cfab45e5
jiawei xie <asdfg756868947@126.com>
2023-12-08 09:07:12 +0800

    This model's maximum context length is 4097 tokens. However, your messages resulted in 4888 tokens. Please reduce the length of the messages.

--- mainSift.py
+++ mainSift.py
@@ -3,6 +3,75 @@ import numpy as np
 import os
 from scipy.spatial import Delaunay
 import open3d as o3d
+import cv2
+import numpy as np
+import os
+import scipy
+from scipy.spatial.distance import cosine
+
+
+# 定义一个函数来找到全局点云中的点与新图像中的特征点之间的匹配
+# 定义一个函数来找到全局点云中的点与新图像中的特征点之间的匹配  3:128维度的匹配， 我现在需要做的是， 进行系统的知识学习
+def find_matches(global_point_cloud, keypoints_new, descriptors_new):
+    matched_3d_points = []  # 存储匹配的全局点云中的点
+    matched_2d_points = []  # 存储匹配的新图像中的特征点
+
+    # 遍历新图像中的关键点和描述子
+    for i, keypoint in enumerate(keypoints_new):
+        descriptor = descriptors_new[i][:128]  # 限制描述子的长度为128以匹配点的维度
+
+        # 初始化最小距离和最近的点
+        min_distance = float('inf')
+        closest_point = None
+
+        # 遍历全局点云中的点
+        for point in global_point_cloud:
+            # 计算描述子之间的距离（使用余弦相似度作为距离度量）
+            distance = cosine(descriptor, point[:128])
+
+            # 如果找到更近的点，则更新最小距离和最近的点
+            if distance < min_distance:
+                min_distance = distance
+                closest_point = point
+
+        # 如果找到了匹配点，则将它们添加到匹配列表中
+        if closest_point is not None:
+            matched_3d_points.append(closest_point)
+            matched_2d_points.append(keypoint.pt)
+
+    return np.array(matched_3d_points), np.array(matched_2d_points)
+
+
+# 剩余部分保持不变
+
+# Bundle Adjustment (BA) 优化
+# 定义BA优化函数
+def bundle_adjustment(camera_poses, point_cloud, K, max_iterations=100):
+    num_cameras = len(camera_poses)
+    num_points = len(point_cloud)
+
+    # 构建优化问题
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, max_iterations, 0.1)
+    flags = cv2.OPTIMIZER_SCHUR
+    optimizer = cv2.UMat(cv2.detail.createBundleAdjuster())
+
+    # 将相机参数和三维点云连接成一个优化问题的参数向量
+    params = np.zeros((3 * num_cameras + 3 * num_points), dtype=np.float32)
+
+    # 初始化优化问题的参数向量
+    for i in range(num_cameras):
+        params[3 * i:3 * i + 3] = cv2.Rodrigues(camera_poses[i][:3, :3])[0]
+        params[3 * num_cameras + 3 * i:3 * num_cameras + 3 * i + 3] = point_cloud[i]
+
+    # 优化相机参数和三维点云
+    (success, params) = optimizer.apply(params, K, criteria, flags)
+
+    # 更新相机姿态和三维点云
+    for i in range(num_cameras):
+        camera_poses[i][:3, :3] = cv2.Rodrigues(params[3 * i:3 * i + 3])[0]
+        point_cloud[i] = params[3 * num_cameras + 3 * i:3 * num_cameras + 3 * i + 3]
+
+    return camera_poses, point_cloud
 
 # 图像文件夹路径和PLY文件保存路径
 img_dir = "C:/Users/crab/PycharmProjects/3dReset/images/"
@@ -18,180 +87,110 @@ K = np.array([[2759.48, 0, 1520.69],
 sift = cv2.SIFT_create()
 
 # 初始化点云和颜色数组
-point_cloud = np.empty((0, 3))
+global_point_cloud = np.empty((0, 3))
+global_camera_poses = []
 point_colors = []
+keypoints_all = []
 
-# 处理所有图片对
-for i in range(5):  # 图片范围从0000.jpg到0004.jpg
-    for j in range(i+1, 5):  # 确保j不超过最后一张图片的索引
-        img_path1 = os.path.join(img_dir, f"{i:04d}.jpg")
-        img_path2 = os.path.join(img_dir, f"{j:04d}.jpg")
+# 选择初始视图 a 和 b
+init_view_a = 0
+init_view_b = 1
 
-        img1 = cv2.imread(img_path1)
-        img2 = cv2.imread(img_path2)
-        if img1 is None or img2 is None:
-            print(f"Warning: Unable to read one of the images: {img_path1} or {img_path2}")
-            continue
+# 选择初始视图的图像路径
+init_img_path_a = os.path.join(img_dir, f"{init_view_a:04d}.jpg")
+init_img_path_b = os.path.join(img_dir, f"{init_view_b:04d}.jpg")
 
-        # 检测关键点并计算描述子
-        keypoints1, descriptors1 = sift.detectAndCompute(img1, None)
-        keypoints2, descriptors2 = sift.detectAndCompute(img2, None)
+img_a = cv2.imread(init_img_path_a)
+img_b = cv2.imread(init_img_path_b)
 
-        # 使用FLANN进行特征匹配
-        FLANN_INDEX_KDTREE = 1
-        index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
-        search_params = dict(checks=50)
-        flann = cv2.FlannBasedMatcher(index_params, search_params)
-        matches = flann.knnMatch(descriptors1, descriptors2, k=2)
+# 检测关键点并计算描述子
+keypoints_a, descriptors_a = sift.detectAndCompute(img_a, None)
+keypoints_b, descriptors_b = sift.detectAndCompute(img_b, None)
 
-        # 筛选好的匹配
-        good_matches = [m for m, n in matches if m.distance < 0.7 * n.distance]
+#加入关键点
+keypoints_all.append(keypoints_a)
+keypoints_all.append(keypoints_b)
 
-        # 保存匹配点对坐标
-        matchedPoints1 = np.float32([keypoints1[m.queryIdx].pt for m in good_matches])
-        matchedPoints2 = np.float32([keypoints2[m.trainIdx].pt for m in good_matches])
+# 使用FLANN进行特征匹配
+FLANN_INDEX_KDTREE = 1
+index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+search_params = dict(checks=50)
+flann = cv2.FlannBasedMatcher(index_params, search_params)
+matches = flann.knnMatch(descriptors_a, descriptors_b, k=2)
 
-        # 进行基本矩阵F的估计并使用RANSAC筛选
-        F, inliers = cv2.findFundamentalMat(matchedPoints1, matchedPoints2, cv2.FM_RANSAC)
+# 筛选好的匹配
+good_matches = [m for m, n in matches if m.distance < 0.7 * n.distance]
 
-        # 筛选内点
-        inlierPoints1 = matchedPoints1[inliers.ravel() == 1]
-        inlierPoints2 = matchedPoints2[inliers.ravel() == 1]
+# 保存匹配点对坐标
+matchedPoints_a = np.float32([keypoints_a[m.queryIdx].pt for m in good_matches])
+matchedPoints_b = np.float32([keypoints_b[m.trainIdx].pt for m in good_matches])
 
-        # 计算本质矩阵E
-        E = K.T @ F @ K
+# 进行基本矩阵F的估计并使用RANSAC筛选
+F, inliers = cv2.findFundamentalMat(matchedPoints_a, matchedPoints_b, cv2.FM_RANSAC)
 
-        # 相机姿态恢复，求解R,t
-        _, R, t, _ = cv2.recoverPose(E, inlierPoints1, inlierPoints2, K)
+# 筛选内点
+inlierPoints_a = matchedPoints_a[inliers.ravel() == 1]
+inlierPoints_b = matchedPoints_b[inliers.ravel() == 1]
 
-        # 创建两个相机的投影矩阵 [R T]
-        proj1 = np.hstack((np.eye(3, 3), np.zeros((3, 1))))
-        proj2 = np.hstack((R, t))
+# 计算本质矩阵E
+E = K.T @ F @ K
 
-        # 转换相机内参矩阵 K 为浮点型
-        fK = K.astype(np.float32)
+# 相机姿态恢复，求解R,t
+_, R_init, t_init, _ = cv2.recoverPose(E, inlierPoints_a, inlierPoints_b, K)
 
-        # 计算投影矩阵 [K * [R|T]]
-        proj1 = fK @ proj1
-        proj2 = fK @ proj2
+# 三角法求解稀疏三维点云
+proj1_init = np.hstack((np.eye(3, 3), np.zeros((3, 1))))
+proj2_init = np.hstack((R_init, t_init))
 
-        # 三角法求解稀疏三维点云
-        point4D_homogeneous = cv2.triangulatePoints(proj1, proj2, inlierPoints1.T, inlierPoints2.T)
-        point3D = cv2.convertPointsFromHomogeneous(point4D_homogeneous.T)
+point4D_homogeneous_init = cv2.triangulatePoints(proj1_init, proj2_init, inlierPoints_a.T, inlierPoints_b.T)
+point3D_init = cv2.convertPointsFromHomogeneous(point4D_homogeneous_init.T)
 
-        # 添加点到点云
-        point_cloud = np.vstack((point_cloud, point3D.reshape(-1, 3)))
+# 添加初始视图的点云到全局点云
+global_point_cloud = np.vstack((global_point_cloud, point3D_init.reshape(-1, 3)))
 
-        # 获取特征点的颜色信息
-        for pt in inlierPoints1:
-            x, y = int(pt[0]), int(pt[1])
-            if 0 <= x < img1.shape[1] and 0 <= y < img1.shape[0]:
-                point_colors.append(img1[y, x])
+# 对后续视图进行处理
+for i in range(2, 5):  # 处理0002.jpg到0004.jpg
+    new_img_path = os.path.join(img_dir, f"{i:04d}.jpg")
+    new_img = cv2.imread(new_img_path)
 
-# 确保点云和颜色数据长度一致
-assert len(point_cloud) == len(point_colors), "点云和颜色数据长度不一致"
+    if new_img is None:
+        print(f"Warning: Unable to read the image: {new_img_path}")
+        continue
 
-# 使用Delaunay三角化来生成三维面片
-if len(point_cloud) > 3:
-    tri = Delaunay(point_cloud)
-    meshes = point_cloud[tri.simplices]
-else:
-    meshes = []
+    # 检测关键点并计算描述子
+    keypoints_new, descriptors_new = sift.detectAndCompute(new_img, None)
 
-# 使用Poisson重建方法
+    # 寻找与全局点云中点的匹配
+    # 这里需要实现一个函数来找到全局点云中的点与新图像中的特征点之间的匹配
+    # 这通常涉及到搜索全局点云中与新图像特征点最接近的点
+    matched_3d_points, matched_2d_points = find_matches(global_point_cloud, keypoints_new, descriptors_new)
 
+    # 使用solvePnP估计新视图的相机姿态
+    _, rvec, tvec = cv2.solvePnP(matched_3d_points, matched_2d_points, K, None)
 
-# 面片扩张和过滤的迭代
-# 定义面片扩张的阈值
-expansion_threshold = 0.1  # 根据需要调整阈值
+    # 转换旋转向量到旋转矩阵
+    R_new, _ = cv2.Rodrigues(rvec)
+    T_new = np.hstack((R_new, tvec.reshape(3, 1)))
 
-# 定义面片过滤的阈值
-filter_threshold = 0.2  # 根据需要调整阈值
+    # 更新全局相机姿态列表
+    global_camera_poses.append(T_new)
 
-# 初始化新的三维面片列表
-new_meshes = []
+    # 可选：使用当前视图更新全局点云
+    # 这可能包括三角测量新的点或更新现有点的位置
 
-# 遍历现有的面片列表
-for triangle in meshes:
-    # 计算当前面片的中心点
-    triangle_center = np.mean(triangle, axis=0)
+# Bundle Adjustment (BA) 优化
+# 这里可以添加BA的代码来优化相机姿态和3D点云
+# 定义BA优化函数
+# 使用BA优化相机姿态和三维点云
+optimized_camera_poses, optimized_point_cloud = bundle_adjustment(
+    global_camera_poses, global_point_cloud, keypoints_all, K, max_iterations=10
+)
+# 清除错误的3D点
+# 这里可以添加代码来检查和清除不合格的3D点
 
-    # 寻找与中心点距离在阈值内的稀疏点云点
-    nearby_points = [point for point in point_cloud if np.linalg.norm(point - triangle_center) < expansion_threshold]
+# 添加更多的视图，并重复找到2D-3D对应点和PnP来估计新视图的R和t，以及一致性集C
+# 这里可以继续添加更多的视图并重复之前的流程，找到新视图的R和t，以及一致性集C
 
-    # 如果找到附近的点，则合并当前面片的点云和附近的稀疏点云点
-    if nearby_points:
-        expanded_triangle = np.vstack((triangle, nearby_points))
-        # ... 进行面片过滤
+# 重复上述过程直到添加了所有视图
 
-
-        # 过滤掉不满足条件的面片
-        if len(expanded_triangle) >= 3:
-            # 计算新面片的法向量
-            normal_vector = np.cross(expanded_triangle[1] - expanded_triangle[0], expanded_triangle[2] - expanded_triangle[0])
-            normal_vector /= np.linalg.norm(normal_vector)
-
-            # 根据法向量和阈值进行面片过滤
-            if np.abs(np.dot(normal_vector, [0, 0, 1])) > filter_threshold:
-                new_meshes.append(expanded_triangle)
-    else:
-        # 如果没有找到附近的点，可以选择跳过当前面片或直接添加到 new_meshes
-        continue  # 或 new_meshes.append(triangle)
-
-
-# 使用Poisson重建方法
-point_cloud_o3d = o3d.geometry.PointCloud()
-point_cloud_o3d.points = o3d.utility.Vector3dVector(point_cloud)
-
-# 计算点云的法向量
-point_cloud_o3d.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.1, max_nn=30))
-
-
-# 执行Poisson重建
-mesh, densities = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(point_cloud_o3d, depth=9)
-
-# 将生成的三维模型与之前的三维面片融合
-for triangle in new_meshes:
-    vertices = o3d.utility.Vector3dVector(triangle)
-    mesh_tri = o3d.geometry.TriangleMesh()
-    mesh_tri.vertices = o3d.utility.Vector3dVector(vertices)
-
-    mesh += mesh_tri
-
-# 可视化结果（可选）
-o3d.visualization.draw_geometries([mesh])
-
-# 保存三维模型为PLY文件
-o3d.io.write_triangle_mesh(ply_save_path, mesh)
-print(f"3D model saved to {ply_save_path}")
-
-# 深度图像的生成
-image_height, image_width = img1.shape[:2]  # 使用第一张图像的尺寸
-depth_image_size = (image_height, image_width)
-initial_depth_value = 1000.0  # 初始深度值
-
-# 创建初始深度图像
-depth_image = np.ones(depth_image_size, dtype=np.float32) * initial_depth_value
-
-# 遍历三维面片列表new_meshes
-for triangle in new_meshes:
-    # 计算当前三维面片的中心点
-    triangle_center = np.mean(triangle, axis=0)
-
-    # 计算三维面片的投影坐标
-    projected_points, _ = cv2.projectPoints(triangle, np.zeros(3), np.zeros(3), K, None)
-
-    # 将投影坐标转换为图像坐标
-    projected_points = projected_points.squeeze().astype(int)
-
-    # 将深度值写入深度图像
-    for i, point in enumerate(projected_points):
-        x, y = point
-        if 0 <= x < depth_image_size[1] and 0 <= y < depth_image_size[0]:
-            current_depth = np.linalg.norm(triangle_center - triangle[i])
-            if current_depth < depth_image[y, x]:
-                depth_image[y, x] = current_depth
-
-# 保存深度图像为PNG文件
-cv2.imwrite(depth_image_file, depth_image)
-print(f"Depth image saved to {depth_image_file}")
+# 最后，你可以保存全局点云和相机姿态供后续使用
